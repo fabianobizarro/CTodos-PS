cc714c35dbc6eaf78b7bcd132c5dc54082fa8115
Fabiano Bizarro <fabianobizarro@outlook.com>
2017-02-13 17:26:45 -0200

    UsuarioPerfilAppService adicionado

---
+++ CartaoTodos/CartaoTodos.Application/Services/UsuarioPerfilAppService.cs
@@ -0,0 +1,21 @@
+﻿using CartaoTodos.Application.Interfaces;
+using CartaoTodos.Application.ViewModels;
+using CartaoTodos.Domain.Entities;
+using CartaoTodos.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartaoTodos.Application.Services
+{
+    public class UsuarioPerfilAppService : BaseAppService<UsuarioPerfilViewModel, UsuarioPerfil>, IUsuarioPerfilAppService
+    {
+        public UsuarioPerfilAppService(IUsuarioPerfilService service)
+            : base(service)
+        {
+
+        }
+    }
+}
