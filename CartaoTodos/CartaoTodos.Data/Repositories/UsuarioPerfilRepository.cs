4ad907f5f26f6ae8adbbd44a3d55684660e44257
Fabiano Bizarro <fabianobizarro@outlook.com>
2017-02-13 17:23:38 -0200

    UsuarioPerfilRepository criado

---
+++ CartaoTodos/CartaoTodos.Data/Repositories/UsuarioPerfilRepository.cs
@@ -0,0 +1,14 @@
+﻿using CartaoTodos.Domain.Entities;
+using CartaoTodos.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartaoTodos.Data.Repositories
+{
+    public class UsuarioPerfilRepository : BaseRepository<UsuarioPerfil>, IUsuarioPerfilRepository
+    {
+    }
+}
