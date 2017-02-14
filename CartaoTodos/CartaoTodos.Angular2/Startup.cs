b41aae332a10ea631598ef4c9accdc760ee9be58
Fabiano Bizarro <fabianobizarro@outlook.com>
2017-02-14 11:48:21 -0200

    StaticFiles adicionado

--- CartaoTodos/CartaoTodos.Angular2/Startup.cs
+++ CartaoTodos/CartaoTodos.Angular2/Startup.cs
@@ -28,10 +28,13 @@ namespace CartaoTodos.Angular2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
