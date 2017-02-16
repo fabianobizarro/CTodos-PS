863cc24f3f578f5e1de25c375607d870d5592653
Fabiano Bizarro <fabianobizarro@outlook.com>
2017-02-16 00:02:32 -0200

    desenvolvimento das telas deo projeto MVC

--- CartaoTodos/CartaoTodos.Mvc/Controllers/UsuariosController.cs
+++ CartaoTodos/CartaoTodos.Mvc/Controllers/UsuariosController.cs
@@ -1,6 +1,8 @@
 ﻿using CartaoTodos.Mvc.Models;
 using CartaoTodos.REST;
 using CartaoTodos.REST.Common;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -19,6 +21,10 @@ namespace CartaoTodos.Mvc.Controllers
         public ActionResult Index()
         {
             var usuarios = _client.ObterUsuarios();
+
+            if (usuarios == null)
+                usuarios = new List<Usuario>();
+
             return View(usuarios);
         }
 
@@ -36,7 +42,7 @@ namespace CartaoTodos.Mvc.Controllers
         {
             if (ModelState.IsValid)
             {
-                model.Usuario.Perfis = model.PerfisUsuario.Select(p => new Perfil() { Id = p }).ToArray();
+                model.Usuario.Perfis = model.PerfisUsuario.Select(p => new Perfil() { Id = p }).ToList();
 
                 _client.AdicionarUsuario(model.Usuario);
                 return RedirectToAction("Index", "Usuarios");
@@ -59,6 +65,60 @@ namespace CartaoTodos.Mvc.Controllers
             else
             {
                 return View(usuario);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            var model = new CadastroUsuarioViewModel();
+            model.Usuario = _client.ObterUsuario(id);
+            
+
+            if (model.Usuario == null)
+            {
+                // TODO: Usuário não existe
+                return RedirectToAction("Index", "Usuarios");
+            }
+            else
+            {
+                model.Perfis = _client.ObterPerfis();
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(CadastroUsuarioViewModel model)
+        {
+            return View();
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            var usuario = _client.ObterUsuario(id);
+
+            if (usuario == null)
+            {
+                return RedirectToAction("Index", "Usuarios");
+            }
+            else
+            {
+                return View(usuario);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Excluir(Usuario model)
+        {
+            try
+            {
+                _client.ExcluirUsuario(model.Id);
+                return RedirectToAction("Index", "Usuarios");
+            }
+            catch (Exception ex)
+            {
+                return View(model);
             }
         }
     }
